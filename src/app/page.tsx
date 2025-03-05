9c6497021b4aa055a72872978bfc7415c9d8d17f
terrydiaz <89354469+terrydiaz@users.noreply.github.com>
2025-03-05 01:52:05 -0300

    page modificada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import GymFlowBanner from '@/components/GymFlowBanner/GymFlowBanner';
 import WhyChooseUs from '@/components/WhyChooseUs/WhyChooseUs';
 import SelectYourActivitie from '@/components/SelectYourActivity/SelectYourActivity';
 
-export function Home() {
+export default function Home() {
   return (
     <div className="bg-primary">
       <GymFlowBanner />
@@ -11,5 +11,3 @@ export function Home() {
     </div>
   );
 }
-
-
